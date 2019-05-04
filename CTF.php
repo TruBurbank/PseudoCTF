b45ec7f32f2577642bc6b92475f7e85376df7adb
TruBurbank <43853232+TruBurbank@users.noreply.github.com>
2019-05-04 19:20:00 +0530

    Update CTF.php

--- CTF.php
+++ CTF.php
@@ -1,30 +1,23 @@
+<?php
+$cookie_name = "flag";
+$token="";
+?>
 <html>
 <head>
   <title>
     Calculator
   </title>
-  <link rel="stylesheet" href="main.css">
+  <link rel="stylesheet" href="des.css">
+</head>
+<body text="white">
+  <center>
 <?php
 class Calcu{
-
   public $arr = array();
-/*  public $x;
-  public $y;
-  function __construct($x,$y)
-  {
-    $this->arr[0]=$x;
-    $this->arr[1]=$y;
-  }*/
 }
 class Expression extends Calcu {
   public $expressionop = '';
-  /*function __tostring()
-  {
-      return $Operator = (string)$this->expressionop;
-
-  }*/
 }
-
 function Add($arr){
 
   $Expression1 = $arr[0];
@@ -39,7 +32,6 @@ function Subtract($arr){
   $add = $Expression1 - $Expression2;
   return $add;
 }
-
 function Multiply($arr){
 
   $Expression1 = $arr[0];
@@ -48,29 +40,21 @@ function Multiply($arr){
   return $add;
 }
 function Division($arr){
-
   $Expression1 = $arr[0];
   $Expression2 = $arr[1];
-
-
+  if ($Expression2 == 0)
+  {
+    echo 'Cannot divide by zero';
+  }
+  else {
     $add = $Expression1 / $Expression2;
-
-
-  return $add;
+    return $add;
+  }
 }
 
+$token = isset($_COOKIE[$cookie_name]) ? $_COOKIE[$cookie_name] : null;
 
-/*$object1->arr[0] = 5;
-$object1->arr[1] = 5;
-$object1->expressionop = 'sum';
-if ( $object1->expressionop == 'sum' );
-{
-  echo $object1->sum();
-  echo '<br>';
-}
-echo '<br>';*/
-$token = isset($_POST['token']) ? $_POST['token'] : null;
-$token1 = urldecode(base64_decode($token));
+$token1 = str_rot13(hex2bin(base64_decode($token)));
 if($token1 == null)
 {
   $object1 = new Expression();
@@ -78,31 +62,37 @@ if($token1 == null)
   $object1->arr[1] = $_POST['num2'];
   $object1->arr;
   $object1->expressionop = (string)$_POST['operator'];
-  $Operator = $object1->expressionop;
-  if ( $object1->expressionop == (string)$_POST['operator'] );
+  if (is_numeric($_POST['num1']) && is_numeric($_POST['num2']))
   {
-
-    echo $Operator($object1->arr);
+    $Operator = $object1->expressionop;
+    if ( $object1->expressionop == (string)$_POST['operator'] )
+    {
+      echo '<h3> The answer is : </h3>';
+      echo '<h4>';
+      echo $Operator($object1->arr);
+      echo '</h4>';
+    }
+    echo '<br>';
+    $token = base64_encode(bin2hex(str_rot13(serialize($object1))));
+    setcookie($cookie_name, $token, time() + (3), "/");
   }
-  echo '<br>';
-  $token = base64_encode(urlencode(serialize($object1)));
-
-  echo '<form action="CTF.php" method="post">';
-  echo '<button name = "token" value ="'.$token.'">';
-  echo '<a href="?action=post&token='.$token.'">Share it</a>';
-  echo '</form>';
+  else
+  {
+    echo '<h3>Please enter a number</h3>';
+  }
 }
-else {
+else
+{
+
   $object1 = unserialize($token1);
   $Operator = (string)$object1->expressionop;
+  echo '<h3> The answer is : </h3>';
+  echo '<h4>';
   echo $Operator($object1->arr);
+  echo '</h4>';
   echo '<br>';
-  $token = base64_encode(urlencode(serialize($object1)));
-
-  echo '<form action="CTF.php" method="post">';
-  echo '<button name = "token" value ="'.$token.'">';
-  echo '<a href="?action=post&token='.$token.'">Share it</a>';
-  echo '</form>';
-
 }
- ?>
+?>
+</center>
+</body>
+<html>
